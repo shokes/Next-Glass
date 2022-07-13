db8cc1559d414b3fad09df884d2cd018f4feb4d2
Oshoke <oyatioshoke@yahoo.com>
2022-07-13 07:27:22 +0100

    cross origin fix

--- pages/Blog.js
+++ pages/Blog.js
@@ -8,7 +8,7 @@ const Blog = ({ articles }) => {
       <Head>
         <title>Blog - Next Glass</title>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
 
         <link
           href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300&display=swap'
